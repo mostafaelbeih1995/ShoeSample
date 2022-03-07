496e6b95851b8558ee968435506036e2ed4d3eb2
Mostafa Elbeih <mte10@mail.aub.edu>
2022-03-08 00:03:23 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running on port 5000");
+});
+
